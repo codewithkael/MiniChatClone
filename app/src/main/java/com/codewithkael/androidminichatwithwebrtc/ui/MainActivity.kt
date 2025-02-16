05b0fb4cd583a2f73d74148cedf1558da7ff0c1a
Masoud Sarabadani <codewithkael@gmail.com>
2025-02-15 21:58:20 -0800

    final

--- app/src/main/java/com/codewithkael/androidminichatwithwebrtc/ui/MainActivity.kt
+++ app/src/main/java/com/codewithkael/androidminichatwithwebrtc/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.codewithkael.androidminichatwithwebrtc.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.codewithkael.androidminichatwithwebrtc.ui.screens.MainScreen
 import com.codewithkael.androidminichatwithwebrtc.ui.theme.AndroidMiniChatWithWebRTCTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,10 +14,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Enable edge-to-edge drawing
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             AndroidMiniChatWithWebRTCTheme {
-                MainScreen()
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .windowInsetsPadding(WindowInsets.systemBars) // Ensures space for top & bottom bars
+                ) {
+                    MainScreen()
+                }
             }
         }
     }
